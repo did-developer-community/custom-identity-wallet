e150cf5b41941884dbe9041bca4b5f51ee67cc41
HayashiyamaHiroshi <adaman3568@gmail.com>
2022-07-21 21:13:04 +0900

    chore: use CredentialDetail at CredentialList

--- src/components/organisms/CredentialList.tsx
+++ src/components/organisms/CredentialList.tsx
@@ -2,7 +2,7 @@ import { Grid } from "@chakra-ui/react";
 import React from "react";
 
 import { Manifest } from "../../types";
-import { CredentialCard } from "../molecules/CredentialCard";
+import { CredentialDetail } from "./CredentialDetail";
 
 export interface CredentialListProps {
   manifests: Manifest[];
@@ -12,7 +12,7 @@ export const CredentialList: React.FC<CredentialListProps> = ({ manifests }) => 
   return (
     <Grid gap={4}>
       {manifests.map((manifest, i) => {
-        return <CredentialCard key={i} manifest={manifest} />; // todo keyにindexはアンチパターン
+        return <CredentialDetail key={i} manifest={manifest} />; // todo keyにindexはアンチパターン
       })}
     </Grid>
   );
