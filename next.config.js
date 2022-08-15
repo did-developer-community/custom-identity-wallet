8ff612868381037b75bcbe1155c84f22408a4408
masaki obayashi <40846038+sinsinpurin@users.noreply.github.com>
2022-08-15 23:10:54 +0900

    feat: support pwa

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public/",
+    disable: process.env.NODE_ENV === "development",
+  },
+});
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 import { AppWrapper } from "../components/utils/AppWrapper";
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
-    <AppWrapper>
-      <Component {...pageProps} />
-    </AppWrapper>
+    <>
+      <Head>
+        <title>Identity Wallet</title>
+      </Head>
+      <AppWrapper>
+        <Component {...pageProps} />
+      </AppWrapper>
+    </>
   );
 };
 export default MyApp;
